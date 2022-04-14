e9a720a044150f443313e3ddfca58be730a67ee0
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-14 08:18:46 +0100

    poyla thinking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 // We have to understand the problem.
+
+
 // 1. What is the unknown?
+return true if there are duplicates
+
 // 2. What are the data inputs?
+a variable number of arguments or [...args]
+
 // 3. Can you restate the problem in your own words?
+checks where there are duplicates in any of the arguments passed in
+
 // 4. Can the unknown be determined from the data inputs?
 // 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory?
 // 6. How should I label important pieces of data input that are a part of the problem?
@@ -9,9 +17,14 @@
 // 8. Separate the various parts of the condition. Can you write them down?
 // Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found.
 // 9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem?
+frequency counter pattern or multiple pointers pattern
+
 // 10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method?
 // 11.    Should you introduce some auxiliary element in order to make its use possible?
 // 12. Start with a simple example. Could you restate the problem? Could you restate it still differently?
+
+areThereDuplicates(1,2,2);
+
 // 13. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs?
 // 14. Progress to more complex examples. What is your updated plan here to find a solution?
 // 15. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
